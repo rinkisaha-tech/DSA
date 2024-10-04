a1cfbef7aebe2abb0c83f2b9d233d9f82d0244d5
Rinki Saha <128579571+rinkisaha-tech@users.noreply.github.com>
2024-10-04 23:17:25 +0530

    Add files via upload

---
+++ array/insertion.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[10] = {12 , 55, 87 , 99 , 57};
+    int i, pos, num, n = 5;
+
+    for(i = 0; i < n; i++){
+        printf(" %d ",arr[i]);
+    }
+
+    printf("\n\n");
+
+    pos = 4, num = 99;
+    n++;
+
+    for(i = n-1; i >= pos; i--){
+        arr[i] = arr[i-1];
+    }
+
+    arr[pos-1] = num;
+
+    for(i = 0; i < n; i++){
+        printf(" %d ",arr[i]);
+    }
+    return 0;
+}
